e5d41aa8a79568506a49f54423cedd1a179feb7b
tomas-cunha <tcunha930@gmail.com>
2022-07-27 12:19:17 -0300

    exercicio02

--- Semana 3/exercicios.js
+++ Semana 3/exercicios.js
@@ -1,3 +1,4 @@
+//exercicio01
 var listaNumeros = [2, 3, 9, 12, 16, 55, 90, 78, 17]
 
 
@@ -6,3 +7,24 @@ listaNumeros.forEach((number, indice) => {
 })
 
 console.log(listaNumeros);
+
+//exercicio02
+var listaConvidados = [
+    { nome: "José", sobrenome: "Carlos" },
+    { nome: "Alessandro", sobrenome: "Viana" },
+    { nome: "Paula", sobrenome: "Souza" },
+    { nome: "Cristian", sobrenome: "Schimit" },
+    { nome: "Beatriz", sobrenome: "Viana" },
+    { nome: "Fernanda", sobrenome: "Silveira" },
+    { nome: "Cláudia", sobrenome: "Torres" },
+    { nome: "Augusto", sobrenome: "Cesar" },
+    { nome: "Noemi", sobrenome: "Nakamura" },
+    { nome: "Pedro", sobrenome: "Lobo" },
+];
+
+let listaEl = document.getElementById('nomes');
+listaConvidados.forEach((pessoa) => {
+    let itemLista = document.createElement('li');
+    itemLista.textContent = `${pessoa.nome} ${pessoa.sobrenome}`;
+    listaEl.appendChild(itemLista);
+})
