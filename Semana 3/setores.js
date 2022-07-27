17de771da1ac82ebedf89b4de2f3966cb0312384
tomas-cunha <tcunha930@gmail.com>
2022-07-27 15:25:38 -0300

    exercicio04

---
+++ Semana 3/setores.js
@@ -0,0 +1,41 @@
+const convidado1 = {
+    nome: 'Tomás',
+    sobrenome: 'Cunha',
+    setor: 'camarote',
+    idade: 26,
+};
+
+const convidado2 = {
+    nome: 'Paula',
+    sobrenome: 'Salum',
+    setor: 'camarote',
+    idade: 27,
+};
+
+const convidado3 = {
+    nome: 'Mário',
+    sobrenome: 'Cherubini',
+    setor: 'pista',
+    idade: 27,
+};
+
+const convidado4 = {
+    nome: 'Klaus',
+    sobrenome: 'Rocha',
+    setor: 'pista',
+    idade: 17,
+};
+
+const convidado5 = {
+    nome: 'Marcos',
+    sobrenome: 'Correa',
+    setor: 'arquibancada',
+    idade: 16,
+};
+
+const convidado6 = {
+    nome: 'Ana',
+    sobrenome: 'Silva',
+    setor: 'arquibancada',
+    idade: 30,
+};
