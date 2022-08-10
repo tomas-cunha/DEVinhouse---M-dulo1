8d2c68d1105462aeb7d8c5af0bbddce7809794fa
tomas-cunha <tcunha930@gmail.com>
2022-08-10 15:58:09 -0300

    exercicio09

--- Semana 4/exercicios.js
+++ Semana 4/exercicios.js
@@ -7,3 +7,28 @@ const parouImpar = () => {
 
 const verificar = document.getElementById('button-number');
 verificar.addEventListener('click', parouImpar);
+
+const exibeMensagem = (msg) => {
+    const elemento = document.getElementById('resultado-age');
+    elemento.innerText = msg;
+}
+
+const classificaIdade = () => {
+    const idade = document.getElementById('age').value;
+
+
+    if (idade <= 15) {
+        exibeMensagem('Você é classificado como jovem!')
+        return;
+    }
+
+    if (idade >= 65) {
+        exibeMensagem('Você é considerado idoso!');
+        return;
+    }
+
+    exibeMensagem('Você é considerado adulto!')
+}
+
+const classificar = document.getElementById('button-age');
+classificar.addEventListener('click', classificaIdade);
