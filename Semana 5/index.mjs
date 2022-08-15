d73da54c9d579ef0d4624929a2928d31cda207c1
tomas-cunha <tcunha930@gmail.com>
2022-08-15 11:58:38 -0300

    exercicio03

--- Semana 5/index.mjs
+++ Semana 5/index.mjs
@@ -5,3 +5,23 @@ import saudacaoEspecial from "./saudacaoespecial.mjs";
 document.querySelector('body').onload = alert(saudacao());
 document.querySelector('body').onload = alert(saudacaoEspecial('Tomás'));
 
+//exercício 03
+
+class Produto {
+    nome;
+    preco;
+    emEstoque;
+    quantidade;
+
+    constructor(nome, preco, emEstoque, quantidade) {
+        this.nome = nome;
+        this.preco = preco;
+        this.emEstoque = emEstoque;
+        this.quantidade = quantidade;
+    }
+}
+
+const shampoo = new Produto('pantene', 100, 5000, 2);
+
+console.log(shampoo);
+
