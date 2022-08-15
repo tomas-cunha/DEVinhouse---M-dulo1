46766ae45e15bea22b0f01b648969d09157ccd6a
tomas-cunha <tcunha930@gmail.com>
2022-08-15 11:44:59 -0300

    exercicio02

--- Semana 5/index.mjs
+++ Semana 5/index.mjs
@@ -1,5 +1,7 @@
 import { saudacao } from "./saudacao.mjs"
+import saudacaoEspecial from "./saudacaoespecial.mjs";
 
 
 document.querySelector('body').onload = alert(saudacao());
+document.querySelector('body').onload = alert(saudacaoEspecial('Tomás'));
 
---
+++ Semana 5/saudacaoespecial.mjs
@@ -0,0 +1,5 @@
+const saudacaoEspecial = (nome) => {
+    return `Olá ${nome}! Seja muito bem vindo!`;
+};
+
+export default saudacaoEspecial;
