4e1a3799aab5bd425e8fffc4c40b6e3e0f4221bb
tomas-cunha <tcunha930@gmail.com>
2022-08-17 16:35:30 -0300

    ex03 - card

--- devinrestaurant/src/App.jsx
+++ devinrestaurant/src/App.jsx
@@ -1,5 +1,6 @@
 
 import './App.css'
+import { Card } from './components/Card/Card'
 import { Footer } from './components/Footer/Footer'
 import { Navbar } from './components/Navbar/Navbar'
 
@@ -9,6 +10,10 @@ function App() {
   return (
     <div className="App">
       <Navbar />
+      <main className='main'>
+        <Card nome='Lasanha' imagem='/lasanha.jpg' descricao='Deliciosa lasanha a bolonhesa' preco={60} tempo={30} />
+
+      </main>
       <Footer />
     </div>
   )
--- devinrestaurant/src/components/Footer/Footer.jsx
+++ devinrestaurant/src/components/Footer/Footer.jsx
@@ -5,33 +5,30 @@ export const Footer = () => {
         <footer className={styles.footer}>
             <div className={styles.footerTop}>
                 <div>
-                    {/* <h3>Endereço:</h3> */}
-                    <div>
-                        <p>Florianópolis - SC</p>
-                        <p>Rua das Delícias, nº 505</p>
-                    </div>
+
+                    <p>Florianópolis - SC</p>
+                    <p>Rua das Delícias, nº 505</p>
+
                 </div>
                 <div>
-                    {/* <h3 className={styles.subtitulo}>Siga a gente:</h3> */}
                     <ul className={styles.ul}>
                         <li>
-                            <a href=""><img src="/instagram.png" width={'30px'} alt="" /></a>
+                            <a href=""><img src="/instagram.png" width={'30px'} alt="instagram logo" /></a>
                         </li>
                         <li>
-                            <a href=""><img src="/twitter.png" width={'30px'} alt="" /></a>
+                            <a href=""><img src="/twitter.png" width={'30px'} alt=" twitter logo" /></a>
                         </li>
                         <li>
-                            <a href=""><img src="/linkedin.png" width={'30px'} alt="" /></a>
+                            <a href=""><img src="/linkedin.png" width={'30px'} alt="linkedin logo" /></a>
                         </li>
                     </ul>
                     <p>© DEVinRestaurant - 2022</p>
                 </div>
                 <div>
-                    {/* <h3>Horário de funcionamento:</h3> */}
-                    <div>
-                        <p>Terça a Quinta: das 19hrs às 23hrs</p>
-                        <p>Sxta a Domingo: das 18hrs às 24hrs</p>
-                    </div>
+
+                    <p>Terça a Quinta: das 19hrs às 23hrs</p>
+                    <p>Sxta a Domingo: das 18hrs às 24hrs</p>
+
                 </div>
             </div>
         </footer>
